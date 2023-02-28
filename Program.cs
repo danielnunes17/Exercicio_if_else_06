2767533f43a771ac4d9080852cf6c35eb801d432
Daniel Stephanio Nunes <dti584@localiza.com>
2023-02-28 11:27:42 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿/*Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
+ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+Se o ponto estiver na origem, escreva a mensagem “Origem”.
+Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a
+situação.*/
+
+
+using System.Globalization;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um valor para X: ");
+        double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Digite um valor para Y: ");
+        double y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        if (x == 0 && y == 0)
+        {
+            Console.WriteLine("Origem");
+        }
+        else if (x == 0.0)
+        {
+            Console.WriteLine("Eixo X");
+        }
+        else if (y == 0.0)
+        {
+            Console.WriteLine("Eixo Y");
+        }
+        else if (x > 0 && y > 0)
+        {
+            Console.WriteLine("Q1");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("Q2");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("Q3");
+        }
+        else
+        {
+            Console.WriteLine("Q4");
+        }
+    }
+}
